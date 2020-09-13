7637c902eee422462baa9202e8ad969b21690a79
satoshi.hashimoto <babolatlist@gmail.com>
2020-09-13 18:05:38 +0900

    RDB設定

--- back/main.py
+++ back/main.py
@@ -1,13 +1,15 @@
 from flask import Flask, render_template
+from api import text_count_bp
 
 app = Flask(__name__, static_folder='../front/dist/static', template_folder='../front/dist')
 # app = Flask(__name__, static_folder='../front/public', template_folder='../front/public')
+app.register_blueprint(text_count_bp)
 
-@app.route('/')
+# @app.route('/')
 
-# @app.route('/', defaults={'path': ''})
-# @app.route('/<path:path>')
-def index():
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def index(path):
     return render_template('index.html')
 
 if __name__ == '__main__':
